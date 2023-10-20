ab2e91194debc6934c8d5b7ab4bb018f3f9f9962
215410006 <145089293+215410006@users.noreply.github.com>
2023-10-20 12:43:43 +0700

    HomeViewModel.kt

--- app/src/main/java/com/riky_215410006/moviejetpacklast/ui/homestart/HomeViewModel.kt
+++ app/src/main/java/com/riky_215410006/moviejetpacklast/ui/homestart/HomeViewModel.kt
@@ -11,9 +11,14 @@ import javax.inject.Inject
 
 class HomeViewModel @Inject constructor(private val movieRepository: MovieRepository) :
     ViewModel() {
+    // Konstruktor yang menerima MovieRepository sebagai dependensi melalui dependency injection
 
-    fun getListPopularMovie(): LiveData<Resource<PagedList<MovieEntity>>> = movieRepository.getPopularMovies()
+    fun getListPopularMovie(): LiveData<Resource<PagedList<MovieEntity>>>
+            = movieRepository.getPopularMovies()
+    // Fungsi ini mengembalikan LiveData yang berisi sumber daya (Resource) yang berisi daftar film populer.
+    // Data ini diambil dari movieRepository.
 
-    fun getListPopularTv(): LiveData<Resource<PagedList<TvEntity>>> = movieRepository.getPopularTv()
-
+    fun getListPopularTv(): LiveData<Resource<PagedList<TvEntity>>>
+            = movieRepository.getPopularTv()
+    // Fungsi ini serupa dengan yang sebelumnya, tetapi digunakan untuk mengambil daftar acara TV populer dari movieRepository.
 }
