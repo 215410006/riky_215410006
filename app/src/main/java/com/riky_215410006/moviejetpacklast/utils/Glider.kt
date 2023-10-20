f6a99aeb165951145fcfc2d4f8b8c0df0cecce96
215410006 <145089293+215410006@users.noreply.github.com>
2023-10-20 12:54:03 +0700

    Glider.kt

--- app/src/main/java/com/riky_215410006/moviejetpacklast/utils/Glider.kt
+++ app/src/main/java/com/riky_215410006/moviejetpacklast/utils/Glider.kt
@@ -6,11 +6,16 @@ import com.bumptech.glide.request.RequestOptions
 import com.riky_215410006.moviejetpacklast.R
 
 fun ImageView.loadFromUrl(path: String) {
+    // Langkah 1: Membersihkan operasi pemrosesan gambar sebelumnya (jika ada).
     Glide.with(this).clear(this)
+
+    // Langkah 2: Memulai operasi pemrosesan gambar dengan menggunakan Glide.
     Glide.with(this)
         .setDefaultRequestOptions(
             RequestOptions()
-            .placeholder(R.drawable.ic_launcher_foreground)
-            .error(R.drawable.ic_launcher_foreground)
-        ).load(path).into(this)
+                .placeholder(R.drawable.ic_launcher_foreground) // Gambar placeholder saat gambar sedang dimuat.
+                .error(R.drawable.ic_launcher_foreground)       // Gambar yang akan ditampilkan jika terjadi kesalahan saat memuat.
+        )
+        .load(path) // Memuat gambar dari URL yang diberikan.
+        .into(this)  // Menampilkan gambar hasil pemrosesan di ImageView ini.
 }
