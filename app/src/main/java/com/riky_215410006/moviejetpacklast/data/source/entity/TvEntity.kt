f33b27c3d1b2640c890298f9090cc8ceb8af2c77
215410006 <145089293+215410006@users.noreply.github.com>
2023-10-20 11:27:44 +0700

    TvEntity.kt

--- app/src/main/java/com/riky_215410006/moviejetpacklast/data/source/entity/TvEntity.kt
+++ app/src/main/java/com/riky_215410006/moviejetpacklast/data/source/entity/TvEntity.kt
@@ -11,26 +11,33 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class TvEntity(
 
+    // Kolom id yang merupakan primary key dan akan di-generate secara otomatis
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id")
     var id: Int? = null,
 
+    // Kolom tvShowId untuk menyimpan ID acara TV
     @NonNull
     @ColumnInfo(name = "tvShowId")
     var tvShowId: Int = 0,
 
+    // Kolom name untuk menyimpan nama acara TV
     @ColumnInfo(name = "tvShowName")
     var name: String? = null,
 
+    // Kolom desc untuk menyimpan deskripsi acara TV
     @ColumnInfo(name = "tvShowDesc")
     var desc: String? = null,
 
+    // Kolom poster untuk menyimpan URL gambar poster acara TV
     @ColumnInfo(name = "tvShowPoster")
     var poster: String? = null,
 
+    // Kolom imgPreview untuk menyimpan URL gambar preview acara TV
     @ColumnInfo(name = "tvShowImgPreview")
     var imgPreview: String? = null,
 
+    // Kolom isFavorite untuk menandai apakah acara TV ini favorit atau tidak
     @NonNull
     @ColumnInfo(name = "isFavorite")
     var isFavorite: Boolean = false
