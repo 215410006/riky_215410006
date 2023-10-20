ac1d4dcd09c47644359487aae80793dd376bd32e
215410006 <145089293+215410006@users.noreply.github.com>
2023-10-20 11:31:07 +0700

    ApiResponse.kt

--- app/src/main/java/com/riky_215410006/moviejetpacklast/data/source/response/ApiResponse.kt
+++ app/src/main/java/com/riky_215410006/moviejetpacklast/data/source/response/ApiResponse.kt
@@ -2,8 +2,10 @@ package com.riky_215410006.moviejetpacklast.data.source.response
 
 class ApiResponse<T>(val status: StatusResponse, val body: T?, val message: String?) {
     companion object {
+        // Membuat objek ApiResponse dengan status SUCCESS dan data yang berhasil diterima.
         fun <T> success(body: T): ApiResponse<T> = ApiResponse(StatusResponse.SUCCESS, body, null)
 
+        // Membuat objek ApiResponse dengan status ERROR, pesan kesalahan, dan data yang mungkin ada.
         fun <T> error(msg: String, body: T): ApiResponse<T> = ApiResponse(StatusResponse.ERROR, body, msg)
     }
 }
