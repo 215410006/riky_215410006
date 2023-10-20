08fbff9c07eada421622adf0861d37c5dff894fb
215410006 <145089293+215410006@users.noreply.github.com>
2023-10-20 11:53:34 +0700

    AppModule.kt

--- app/src/main/java/com/riky_215410006/moviejetpacklast/di/home/AppModule.kt
+++ app/src/main/java/com/riky_215410006/moviejetpacklast/di/home/AppModule.kt
@@ -14,40 +14,43 @@ import javax.inject.Singleton
 
 @Module
 class AppModule {
-
     companion object {
-
+        // Mendefinisikan penyediaan MovieDatabase sebagai singleton
         @Singleton
         @Provides
         fun provideMovieDatabase(application: Application): MovieDatabase =
-                MovieDatabase.getInstance(application)
+            MovieDatabase.getInstance(application)
 
+        // Mendefinisikan penyediaan MovieDao sebagai singleton
         @Singleton
         @Provides
         fun provideMovieDao(movieDatabase: MovieDatabase): MovieDao =
-                movieDatabase.movieDao()
+            movieDatabase.movieDao()
 
+        // Mendefinisikan penyediaan LocalSource sebagai singleton
         @Singleton
         @Provides
         fun provideLocalSource(movieDao: MovieDao): LocalSource =
-                LocalSource(movieDao)
+            LocalSource(movieDao)
 
+        // Mendefinisikan penyediaan RemoteDataSource sebagai singleton
         @Singleton
         @Provides
         fun provideRemoteDataSource(apiService: ApiService): RemoteDataSource =
-                RemoteDataSource(apiService)
+            RemoteDataSource(apiService)
 
+        // Mendefinisikan penyediaan MovieRepository sebagai singleton
         @Singleton
         @Provides
         fun provideMovieRepository(
-                remoteDataSource: RemoteDataSource,
-                localDataSource: LocalSource
+            remoteDataSource: RemoteDataSource,
+            localDataSource: LocalSource
         ): MovieRepository = MovieRepository(remoteDataSource, localDataSource)
 
+        // Mendefinisikan penyediaan ViewModelFactory sebagai singleton
         @Singleton
         @Provides
         fun provideViewModelFactory(catalogRepository: MovieRepository): ViewModelFactory =
-                ViewModelFactory(catalogRepository)
-
+            ViewModelFactory(catalogRepository)
     }
 }
