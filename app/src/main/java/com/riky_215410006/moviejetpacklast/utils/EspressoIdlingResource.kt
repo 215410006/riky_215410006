117354f87c413844947dbb11f2f7f1975ecc6b93
215410006 <145089293+215410006@users.noreply.github.com>
2023-10-20 12:53:30 +0700

    EspressoIdlingResource.kt

--- app/src/main/java/com/riky_215410006/moviejetpacklast/utils/EspressoIdlingResource.kt
+++ app/src/main/java/com/riky_215410006/moviejetpacklast/utils/EspressoIdlingResource.kt
@@ -3,13 +3,18 @@ package com.riky_215410006.moviejetpacklast.utils
 import androidx.test.espresso.idling.CountingIdlingResource
 
 object EspressoIdlingResource {
+    // Nama unik untuk sumber daya idling
     private const val RESOURCE = "GLOBAL"
+
+    // Membuat instance dari CountingIdlingResource
     val espressoTestIdlingResource = CountingIdlingResource(RESOURCE)
 
+    // Fungsi untuk menambah hitungan sumber daya idling
     fun increment() {
         espressoTestIdlingResource.increment()
     }
 
+    // Fungsi untuk mengurangi hitungan sumber daya idling
     fun decrement() {
         espressoTestIdlingResource.decrement()
     }
