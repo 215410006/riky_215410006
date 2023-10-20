641c6b05d189c0bf8855076cf8ebe90a54eb033f
215410006 <145089293+215410006@users.noreply.github.com>
2023-10-20 11:56:19 +0700

    NetworkModule.kt

--- app/src/main/java/com/riky_215410006/moviejetpacklast/di/home/NetworkModule.kt
+++ app/src/main/java/com/riky_215410006/moviejetpacklast/di/home/NetworkModule.kt
@@ -12,8 +12,9 @@ import javax.inject.Singleton
 
 @Module
 class NetworkModule {
-    companion object{
+    companion object {
 
+        // Menyediakan instance OkHttpClient yang digunakan untuk mengonfigurasi koneksi jaringan.
         @Singleton
         @Provides
         fun provideHttpClient(): OkHttpClient = OkHttpClient.Builder().apply {
@@ -22,14 +23,16 @@ class NetworkModule {
             writeTimeout(30, TimeUnit.SECONDS)
         }.build()
 
+        // Menyediakan instance Retrofit yang digunakan untuk berinteraksi dengan API jaringan.
         @Singleton
         @Provides
         fun provideRetrofitInstance(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder().apply {
-            baseUrl(BuildConfig.BASE_URL_TMDB)
-            client(okHttpClient)
-            addConverterFactory(GsonConverterFactory.create())
+            baseUrl(BuildConfig.BASE_URL_TMDB) // Mengatur base URL dari layanan API
+            client(okHttpClient) // Menggunakan OkHttpClient yang telah disediakan sebagai dependensi
+            addConverterFactory(GsonConverterFactory.create()) // Menggunakan GsonConverterFactory untuk mengonversi JSON
         }.build()
 
+        // Menyediakan instance ApiService yang digunakan untuk membuat panggilan ke API jaringan.
         @Provides
         fun provideCatalogApi(retrofit: Retrofit): ApiService = retrofit.create(ApiService::class.java)
     }
