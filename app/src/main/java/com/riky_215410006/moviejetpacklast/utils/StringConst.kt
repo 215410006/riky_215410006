aa5225764598f792a4f53a654c31b5313e5a5058
215410006 <145089293+215410006@users.noreply.github.com>
2023-10-20 12:54:28 +0700

    StringConst.kt

--- app/src/main/java/com/riky_215410006/moviejetpacklast/utils/StringConst.kt
+++ app/src/main/java/com/riky_215410006/moviejetpacklast/utils/StringConst.kt
@@ -1,8 +1,15 @@
 package com.riky_215410006.moviejetpacklast.utils
 
 object StringConst {
+    // Tipe konstanta untuk film
     const val TYPE_MOVIE = "TYPE_MOVIE"
+
+    // Tipe konstanta untuk acara TV
     const val TYPE_TVSHOW = "TYPE_TVSHOW"
+
+    // Ukuran gambar poster dengan lebar 185 piksel
     const val ENDPOINT_POSTER_SIZE_W185 = "w185"
+
+    // Ukuran gambar poster dengan lebar 780 piksel
     const val ENDPOINT_POSTER_SIZE_W780 = "w780"
 }
