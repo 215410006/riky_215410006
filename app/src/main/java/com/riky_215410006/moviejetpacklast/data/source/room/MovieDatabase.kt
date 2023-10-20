938b8ae9471a942e85d6e722ab8c8c23c8ea4b9a
215410006 <145089293+215410006@users.noreply.github.com>
2023-10-20 11:38:39 +0700

    MovieDatabase.kt

--- app/src/main/java/com/riky_215410006/moviejetpacklast/data/source/room/MovieDatabase.kt
+++ app/src/main/java/com/riky_215410006/moviejetpacklast/data/source/room/MovieDatabase.kt
@@ -9,12 +9,15 @@ import com.riky_215410006.moviejetpacklast.data.source.entity.TvEntity
 
 @Database(entities = [MovieEntity::class, TvEntity::class], version = 1, exportSchema = false)
 abstract class MovieDatabase : RoomDatabase() {
+
+    // Mendefinisikan metode abstrak yang memberikan akses ke objek MovieDao.
     abstract fun movieDao(): MovieDao
 
     companion object {
         @Volatile
         private var INSTANCE: MovieDatabase? = null
 
+        // Metode getInstance digunakan untuk mendapatkan atau membuat instans dari basis data lokal.
         fun getInstance(context: Context): MovieDatabase =
             INSTANCE ?: synchronized(this) {
                 INSTANCE ?: Room.databaseBuilder(
