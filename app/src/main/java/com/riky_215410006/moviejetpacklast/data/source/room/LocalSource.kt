df2345ff0a7c2d94bcb02a512f525bfc106f69b6
215410006 <145089293+215410006@users.noreply.github.com>
2023-10-20 11:37:15 +0700

    LocalSource.kt

--- app/src/main/java/com/riky_215410006/moviejetpacklast/data/source/room/LocalSource.kt
+++ app/src/main/java/com/riky_215410006/moviejetpacklast/data/source/room/LocalSource.kt
@@ -8,27 +8,37 @@ import javax.inject.Inject
 
 class LocalSource @Inject constructor(private val movieDao: MovieDao) {
 
+    // Mendapatkan daftar film dari basis data lokal.
     fun getListMovies() : DataSource.Factory<Int, MovieEntity> = movieDao.getListMovies()
 
+    // Mendapatkan daftar film favorit dari basis data lokal.
     fun getListFavoriteMovies() : DataSource.Factory<Int, MovieEntity> = movieDao.getListFavoriteMovies()
 
+    // Mendapatkan daftar acara TV dari basis data lokal.
     fun getListTv() : DataSource.Factory<Int, TvEntity> = movieDao.getListTv()
 
+    // Mendapatkan daftar acara TV favorit dari basis data lokal.
     fun getListFavoriteTv() : DataSource.Factory<Int, TvEntity> = movieDao.getListFavoriteTv()
 
+    // Mendapatkan detail film berdasarkan ID dari basis data lokal.
     fun getDetailMovie(movieId: Int) : LiveData<MovieEntity> = movieDao.getDetailMovieById(movieId)
 
+    // Mendapatkan detail acara TV berdasarkan ID dari basis data lokal.
     fun getDetailTv(tvShowId: Int) : LiveData<TvEntity> = movieDao.getDetailTvById(tvShowId)
 
+    // Memasukkan daftar film ke basis data lokal.
     fun insertMovies(movies: List<MovieEntity>) = movieDao.insertMovies(movies)
 
+    // Memasukkan daftar acara TV ke basis data lokal.
     fun insertTv(tvShows: List<TvEntity>) = movieDao.insertTv(tvShows)
 
+    // Menandai atau menghapus tanda favorit dari film.
     fun setFavoriteMovie(movie : MovieEntity) {
         movie.isFavorite = !movie.isFavorite
         movieDao.updateMovie(movie)
     }
 
+    // Menandai atau menghapus tanda favorit dari acara TV.
     fun setFavoriteTv(tvShow : TvEntity) {
         tvShow.isFavorite = !tvShow.isFavorite
         movieDao.updateTv(tvShow)
